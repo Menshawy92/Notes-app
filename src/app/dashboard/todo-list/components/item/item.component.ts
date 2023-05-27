3886e68b4fb120c64beed506e7dccb5b739da030
Menshawy <madel5848@gmail.com>
2023-05-27 17:24:58 +0300

    get all data is done

--- src/app/dashboard/todo-list/components/item/item.component.ts
+++ src/app/dashboard/todo-list/components/item/item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
 })
 export class ItemComponent implements OnInit {
-
+  @Input() tasks: [] | any;
   constructor() { }
 
   ngOnInit() {
-    
+
   }
 
 }
--- src/app/dashboard/todo-list/components/list-todo/list-todo.component.ts
+++ src/app/dashboard/todo-list/components/list-todo/list-todo.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksService } from '../../services/tasks.service';
 import { filter, first, pipe, Subscription } from 'rxjs';
+import { listTodo } from '../../models/toDoList';
 @Component({
   selector: 'app-list-todo',
   templateUrl: './list-todo.component.html',
 })
 export class ListTodoComponent implements OnInit {
   subscription: Subscription | any;
+  allTasks: listTodo[] | any
 
   constructor(private listService: TasksService) { }
 
   ngOnInit() {
+    this.getAllTasks()
+  }
+
+  getAllTasks() {
     this.subscription = this.listService.listTask().subscribe({
       next: (data) => {
-        console.log("data", data)
+        this.allTasks = data
       },
       error: (error) => {
         console.log("error", error) // will stop stream
